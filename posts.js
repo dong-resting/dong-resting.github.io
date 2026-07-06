9f1f81a13ab481d72efc7ee07ec8ff4804f3d37c
dong-resting <3216249724@qq.com>
2026-07-06 20:04:25 +0800

    Replace second article with a suggestion for desktop site

--- posts.js
+++ posts.js
@@ -1,6 +1,6 @@
 const articles = [
   { date: "2026-07-06", title: "这是我的个人博客", excerpt: "我的个人博客今日正式开通，欢迎来到这里，后续会更新更多内容，敬请期待........", img: "img/头像.jpg" ,color:"#48C9B0"},
-  { date: "2026-07-06", title: "二", excerpt: "这是第2个文章", img: "" },
+  { date: "2026-07-06", title: "建议访问电脑版网页", excerpt: "由于手机端显示效果问题，建议访问电脑版网页", img: "" },
   { date: "2026-07-06", title: "一", excerpt: "这是第1个文章", img: "" },
   { date: "2026-07-01", title: "233", excerpt: "23333333333333333333333333333333", img: "" },
   { date: "2026-07-01", title: "xixi", excerpt: "被发现了...", img: "https://ts4.tc.mm.bing.net/th/id/OIP-C.yGJ1p7sl4ccWGj_WWVgvogHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" },
