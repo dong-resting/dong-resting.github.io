2c3485afa59e94220975d9386089c9e6d7ea568d
dong-resting <3216249724@qq.com>
2026-07-06 19:15:58 +0800

    Fix formatting of articles array in posts.js

--- posts.js
+++ posts.js
@@ -1,4 +1,4 @@
-const articles = [ 
+const articles = [
   { date: "2026-07-06", title: "这是我的个人博客", excerpt: "我的个人博客今日正式开通，欢迎来到这里，后续会更新更多内容，敬请期待........", img: "img/头像.jpg" ,color:"#48C9B0"},
   { date: "2026-07-06", title: "手机端建议访问电脑版", excerpt: "手机端建议访问电脑版", img: "" },
   { date: "2026-07-06", title: "一", excerpt: "这是第1个文章", img: "" },
