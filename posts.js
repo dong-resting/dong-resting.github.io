07be9e9875c5b4a3d25dbdf1731a18f89e1b2eaa
dong-resting <3216249724@qq.com>
2026-07-06 17:49:20 +0800

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,7 @@
+const articles = [
+  { date: "2026-07-06", title: "这是我的个人博客", excerpt: "我的个人博客今日正式开通，欢迎来到这里，后续会更新更多内容，敬请期待........", img: "img/头像.jpg" ,color:"#48C9B0"},
+  { date: "2026-07-06", title: "二", excerpt: "这是第2个文章", img: "" },
+  { date: "2026-07-06", title: "一", excerpt: "这是第1个文章", img: "" },
+  { date: "2026-07-01", title: "233", excerpt: "23333333333333333333333333333333", img: "" },
+  { date: "2026-07-01", title: "xixi", excerpt: "被发现了...", img: "https://ts4.tc.mm.bing.net/th/id/OIP-C.yGJ1p7sl4ccWGj_WWVgvogHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" },
+];
